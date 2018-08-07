f93690741be4ffa024b01e15d851e3ef5ad82a94
Pavel Maslov <pavel.maslov@ijs.si>
2018-08-07 16:52:16 +0200

    moved everything to a class/object

--- aplouder.js
+++ aplouder.js
@@ -1,114 +1,129 @@
-var Filez = [];
+function Aplouder(options) {
+    var self = this;
 
-// using file manager after button click
-document.querySelector('.inputfile').addEventListener('change', function (e) {
-    removeGallery();
-    processFiles(this.files);
-}, false);
-
-
-var dropArea = document.getElementById("ap-droparea");
-
-function clickSelectFilesButton() {
-    document.getElementsByClassName("ap-message")[0].onclick = function () {
-        document.getElementById("images").click();
+    this.getDropAreaEl = function () {
+        return document.getElementById("ap-droparea");
     };
-}
 
-clickSelectFilesButton();
+    this.getGalleryEl = function () {
+        return document.getElementById("ap-gallery");
+    };
 
-function processFiles(files, callback = null) {
-    for (let i = 0; i < files.length; i++) {
-        let src = files[i];
-        file2base64(src, function (file64) {
-            scaleImage(file64, 120, 120, function (scaledImg) {
-                drawImage(scaledImg, {name: src.name, size: src.size});
-                obj = {srcFile: src, base64: file64, thumb: scaledImg};
-                Filez.push(obj);
-                if (callback) callback(obj);
-                clickSelectFilesButton();
+    this.getMessageEl = function () {
+        return document.getElementById("ap-message");
+    };
+
+    this.getInputFileEl = function () {
+        return document.querySelector('.inputfile');
+    };
+
+    this.initBrowseFilesButton = function () {
+        this.getMessageEl().onclick = function () {
+            document.getElementById("images").click();
+        };
+    };
+
+    this.processFiles = function (files, callback = null) {
+        for (let i = 0; i < files.length; i++) {
+            let src = files[i];
+            this.file2base64(src, function (file64) {
+                self.scaleImage(file64, 120, 120, function (scaledImg) {
+                    self.drawImage(scaledImg, {name: src.name, size: src.size});
+                    obj = {srcFile: src, base64: file64, thumb: scaledImg};
+                    self.Filez.push(obj);
+                    if (callback) callback(obj);
+                    self.initBrowseFilesButton();
+                });
             });
-        });
-    }
-}
-
-function file2base64(file, callback) {
-    var reader = new FileReader();
-    reader.onload = function (e) {
-        callback(e.target.result);
-    };
-    reader.readAsDataURL(file);
-}
-
-function scaleImage(url, width, height, callback){
-    var img = new Image();
-    img.crossOrigin = "anonymous";
-
-    // When the image is loaded, resize it in canvas.
-    img.onload = function() {
-        var canvas = document.createElement("canvas"),
-            ctx = canvas.getContext("2d");
-
-        canvas.width = width;
-        canvas.height= height;
-
-        // draw the img into canvas
-        if (img.width === img.height)
-            ctx.drawImage(this, 0, 0, width, height);
-        else {
-            minVal = Math.min(img.width, img.height);
-            if (img.width > img.height)
-                ctx.drawImage(this, (img.width - minVal) / 2, 0, minVal, minVal, 0, 0, width, height);
-            else
-                ctx.drawImage(this, 0, (img.height - minVal) / 2, minVal, minVal, 0, 0, width, height);
         }
-
-        callback(canvas.toDataURL("image/png"));
     };
 
-    img.src = url;
+    this.file2base64 = function (file, callback) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            callback(e.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
+
+    this.scaleImage = function (url, width, height, callback) {
+        var img = new Image();
+        img.crossOrigin = "anonymous";
+
+        // When the image is loaded, resize it in canvas.
+        img.onload = function () {
+            var canvas = document.createElement("canvas"),
+                ctx = canvas.getContext("2d");
+
+            canvas.width = width;
+            canvas.height = height;
+
+            // draw the img into canvas
+            if (img.width === img.height)
+                ctx.drawImage(this, 0, 0, width, height);
+            else {
+                minVal = Math.min(img.width, img.height);
+                if (img.width > img.height)
+                    ctx.drawImage(this, (img.width - minVal) / 2, 0, minVal, minVal, 0, 0, width, height);
+                else
+                    ctx.drawImage(this, 0, (img.height - minVal) / 2, minVal, minVal, 0, 0, width, height);
+            }
+
+            callback(canvas.toDataURL("image/png"));
+        };
+
+        img.src = url;
+    };
+
+    this.drawImage = function (content, details) {
+        self.getGalleryEl().innerHTML +=
+            '<div class="ap-preview">' +
+            '  <div class="ap-image">' +
+            '     <img>' +
+            '  </div>' +
+            '  <div class="ap-overlay">' +
+            '    <div class="ap-details">' +
+            '       <div class="ap-size">' +
+            self.formatFileSize(details.size) +
+            '       </div>' +
+            '       <div class="ap-name">' +
+            details.name +
+            '       </div>' +
+            '    </div>' +
+            '  </div>' +
+            '</div>';
+        this.getDropAreaEl().querySelector("div.ap-preview:last-child div.ap-image img").setAttribute('src', content);
+    };
+
+    this.removeGallery = function () {
+        document.getElementById("ap-gallery").innerHTML = "";
+        Filez = [];
+    };
+
+    this.formatFileSize = function (bytes) {
+        if (bytes < 1000) return bytes + " bytes";
+        if (bytes >= 1000 && bytes <= 1000 * 1000) return Math.round(bytes / 1000) + " kB";
+        return Math.round(bytes / 1000000 * 10) / 10 + " MB";
+    };
+
+    this.preventDefaults = function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    };
 }
 
-function drawImage(content, details) {
-    document.getElementById("gallery").innerHTML +=
-        '<div class="ap-preview">' +
-        '  <div class="ap-image">' +
-        '     <img>' +
-        '  </div>' +
-        '  <div class="ap-overlay">' +
-        '    <div class="ap-details">' +
-        '       <div class="ap-size">' +
-                    formatFileSize(details.size) +
-        '       </div>' +
-        '       <div class="ap-name">' +
-                    details.name +
-        '       </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>';
-    dropArea.querySelector("div.ap-preview:last-child div.ap-image img").setAttribute('src', content);
-}
 
-function removeGallery() {
-    document.getElementById("gallery").innerHTML = "";
-    Filez = [];
-}
+Aplouder.prototype.init = function () {
+    var self = this;
+    this.Filez = [];
+    this.initBrowseFilesButton();
 
-function formatFileSize(bytes) {
-    if (bytes < 1000) return bytes + " bytes";
-    if (bytes >= 1000 && bytes <= 1000*1000) return Math.round(bytes / 1000) + " kB";
-    return Math.round(bytes / 1000000 * 10) / 10 + " MB";
-}
+    // using file manager after button click
+    this.getInputFileEl().addEventListener('change', function (e) {
+        self.removeGallery();
+        self.processFiles(this.files);
+    }, false);
+};
 
-function preventDefaults(e) {
-    e.preventDefault();
-    e.stopPropagation();
-}
 
-function highlight(e) {
-    dropArea.classList.add('highlight');
-}
 
-function unhighlight(e) {
-    dropArea.classList.remove('highlight');
-}
